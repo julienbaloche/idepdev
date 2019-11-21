8d4de55cbe058569091e6f38fdb03ab925bf2e5b
julienbaloche <julienbaloche-rousseau@hotmail.com>
2019-11-21 13:32:40 +0100

    beginning of the security

--- src/main/java/com/idep/api/ApiApplication.java
+++ src/main/java/com/idep/api/ApiApplication.java
@@ -1,7 +1,14 @@
 package com.idep.api;
 
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class ApiApplication {
@@ -9,5 +16,21 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		// Enable SSL Trafic
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+	}
 
-}
+
+	}
---
+++ src/main/java/com/idep/api/security/UserPrincipalDetailsService.java
@@ -0,0 +1,27 @@
+package com.idep.api.security;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.idep.api.model.User;
+import com.idep.api.db.UserRepository;
+
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(s);
+
+        return new UserPrincipal(user);
+    }
+}
