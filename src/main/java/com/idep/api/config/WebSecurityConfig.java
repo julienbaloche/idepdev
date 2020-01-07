b7904b7c31c29bbc4a53a513082ec0bff9af7014
julienbaloche <julienbaloche-rousseau@hotmail.com>
2020-01-07 16:11:54 +0100

    test2

--- src/main/java/com/idep/api/config/WebSecurityConfig.java
+++ src/main/java/com/idep/api/config/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
-    protected void configure(HttpSecurity httpSecurity, HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
         // dont authenticate this particular request
@@ -64,6 +64,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         // Add a filter to validate the tokens with every request
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-        http.cors().and();
+        httpSecurity.cors().and();
+
     }
+
 }
