6df3928f729b8b10b5b4c5f5fd3ed3283c2b2ce7
Matthieu Jacquand <matthieu.jacquand@gmail.com>
2020-01-12 13:46:50 +0100

    ajout cross origin

--- src/main/java/com/idep/api/transaction/TransactionController.java
+++ src/main/java/com/idep/api/transaction/TransactionController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController {
