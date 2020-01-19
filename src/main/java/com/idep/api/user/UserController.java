cbd9033ba39e4d16b6f2263236c1499246658123
Matthieu Jacquand <matthieu.jacquand@gmail.com>
2020-01-19 16:48:28 +0100

    ajout cors

--- src/main/java/com/idep/api/user/UserController.java
+++ src/main/java/com/idep/api/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
