0e255026cb5831d564143e087efd72f3d07f406e
Andrew Pouret <apouret@juniorisep.com>
2020-01-20 12:11:04 +0100

    added image upoad to S3 for Objects

--- src/main/java/com/idep/api/object/ObjectController.java
+++ src/main/java/com/idep/api/object/ObjectController.java
@@ -7,21 +7,24 @@ import com.idep.api.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/objects")
 public class ObjectController {
 
     private final ObjectRepository objectRepository;
     private final UserRepository userRepository;
+    private final ImageService imageService;
 
     @Autowired
-    public ObjectController(ObjectRepository objectRepository, UserRepository userRepository) {
+    public ObjectController(ObjectRepository objectRepository, UserRepository userRepository, ImageService imageService) {
         this.objectRepository = objectRepository;
         this.userRepository = userRepository;
+        this.imageService = imageService;
     }
 
     //GETs
@@ -51,11 +54,11 @@ public class ObjectController {
     @PostMapping("/modif/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Object editObjectById(@PathVariable("id") long objectId,
-                                 @RequestParam("title")String title,
-                                 @RequestParam("author")String author,
-                                 @RequestParam("description")String description,
-                                 @RequestParam("category")ObjectCategory category,
-                                 @RequestParam("price")float price) {
+                                 @RequestParam("title") String title,
+                                 @RequestParam("author") String author,
+                                 @RequestParam("description") String description,
+                                 @RequestParam("category") ObjectCategory category,
+                                 @RequestParam("price") float price) {
         Object object = this.objectRepository.findById(objectId).orElseThrow(ResourceNotFoundException::new);
         object.setTitle(title);
         object.setAuthor(author);
@@ -66,22 +69,43 @@ public class ObjectController {
     }
 
 
+    //IMAGE UPLOAD
+    @PostMapping("/{id}/uploadFile")
+    public Object uploadFile(@PathVariable("id") long objectId, @RequestPart(value = "file") MultipartFile file) {
+        Object object = this.objectRepository.findById(objectId).orElseThrow(ResourceNotFoundException::new);
 
+        String fileUrl = this.imageService.uploadFile(file);
+
+        object.setImageUrl(fileUrl);
+        return this.objectRepository.save(object);
+    }
+
+    //IMAGE DELETE
+    @DeleteMapping("/{id}/deleteFile")
+    public Object deleteFile(@PathVariable("id") long objectId) throws Exception {
+        Object object = this.objectRepository.findById(objectId).orElseThrow(ResourceNotFoundException::new);
+        boolean isDeleted = this.imageService.deleteFileFromS3Bucket(object.getImageUrl());
+
+        if (isDeleted) {
+
+            object.setImageUrl(null);
+            return objectRepository.save(object);
+        }
+        throw new Exception("Error while deleting");
+    }
 
 
     //POSTs
-
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public Object addObject(@RequestBody Object newObject,
-                              @RequestParam("ownerId") long ownerId) {
+                            @RequestParam("ownerId") long ownerId) {
         User owner = this.userRepository.findById(ownerId).orElseThrow(ResourceNotFoundException::new);
         newObject.setOwner(owner);
         return this.objectRepository.save(newObject);
     }
-    //DELETEs
 
-    @CrossOrigin(origins="*")
+    //DELETEs
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Object deleteObjectById(@PathVariable("id") long objectId) {
